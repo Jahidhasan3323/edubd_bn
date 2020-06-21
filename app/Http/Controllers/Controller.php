8ef11ffae3cf0f8948fa51efd575045c59cefd3b
Mohammad Al Arfin (Rokon) <46133517+alarfin@users.noreply.github.com>
2020-06-21 12:59:09 +0600

    SMS Report Update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -94,13 +94,13 @@ class Controller extends BaseController
     protected function get_balance($api_key, $sender_id){
          $url = "http://sms.worldehsan.org/api/sms_balance?api_key=".$api_key."&sender_id=".$sender_id;
          return file_get_contents($url);
-         
+
          /*$ch_banpage = curl_init($url);
          curl_setopt($ch_banpage, CURLOPT_URL, $url);
          curl_setopt($ch_banpage, CURLOPT_HEADER, 0);
          curl_setopt($ch_banpage, CURLOPT_RETURNTRANSFER, true);
          $curl_scraped_page = curl_exec($ch_banpage);
-         curl_close($ch_banpage);*/ 
+         curl_close($ch_banpage);*/
     }
 
     protected function sms_send_by_api($school,$mobile_number,$message){
@@ -156,4 +156,17 @@ class Controller extends BaseController
 
            return response()->json($response, $code);
        }
+
+       function curl_get_file_contents($URL)
+       {
+           $c = curl_init();
+           curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+           curl_setopt($c, CURLOPT_URL, $URL);
+           $contents = curl_exec($c);
+           curl_close($c);
+
+           if ($contents) return $contents;
+           else return FALSE;
+       }
+       
 }
